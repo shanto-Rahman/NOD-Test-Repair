a7b55f9a7e6123b28ac0e5bfb943a7eb9c211ce7
SHANTO <bit0321@iit.du.ac.bd>
2025-01-16 11:52:38 -0600

    updating code

---
+++ data/README.md
@@ -0,0 +1,1 @@
+coverage_data_NOD.csv and coverage_data_OD.csv are from Flapy (SSFL -paper's dataset)
--- run_tests.sh
+++ run_tests.sh
@@ -5,11 +5,16 @@ if [[ $1 == "" ]]; then
 fi
 
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+coverage_Dir="$currentDir/coverages"
+if [ ! -d "$coverage_Dir" ]; then
+    mkdir ${coverage_Dir}
+fi
 
 inputProj=$currentDir"/projects"
 if [ ! -d "projects" ]; then
     mkdir ${inputProj}
 fi
+
 logs="$currentDir/logs"
 if [ ! -d "$currentDir/logs" ]; then
     mkdir "$currentDir/logs"
@@ -37,33 +42,50 @@ while IFS= read -r line
     test_name_only=$(echo $test_name_with_classname | rev | cut -d':' -f1 | rev)
     rootProj=$(echo "$git_link" | rev | cut -d/ -f 2| rev)
     subProj=$(echo "$git_link" | rev | cut -d/ -f 1 | rev)
-    if [[ ! -d ${inputProj}/${rootProj} ]]; then
-        echo git clone "$git_link" $inputProj/$subProj
-        git clone "$git_link" $inputProj/$subProj
+    if [[ ! -d ${inputProj}/${subProj} ]]; then
+        echo "not found ${inputProj}/${subProj}"
+        continue
+    #    echo git clone "$git_link" $inputProj/$subProj
+    #    git clone "$git_link" $inputProj/$subProj
     fi
     cd "$inputProj/$subProj"
     echo "$inputProj/$subProj"
     git checkout ${sha}
-    for i in {1..1000}; do  
+    python3 $currentDir/modify_tox_to_get_cov.py
+    #tox_ini_found=$(find . -name "tox.ini"| wc -l)
+    #if [[ $tox_ini_found -gt 0 ]]; then
+    #    echo "${git_link},$sha" >> "$Results/Tox_found.txt"
+    #    cd $currentDir 
+    #else
+    #     echo "${git_link},$sha" >> "$Results/Tox_not_found.txt"
+    #     cd $currentDir
+    #     rm -rf ${inputProj}/${subProj} 
+    #fi
+    for i in {1..800}; do  
 
-        echo $(pwd)
-        echo "pytest $test_name_with_classname"
-        pytest ${test_name_with_classname}  > "$logs/${test_name_only}_$i.txt"
+        #echo $(pwd)
+        #echo "pytest $test_name_with_classname"
+        #pytest ${test_name_with_classname}  > "$logs/${test_name_only}_$i.txt"
+        #echo "tox -e py36 -- ${test_name_with_classname}"
+        tox -e py36 -- ${test_name_with_classname}  > "$logs/${test_name_only}_$i.txt"
         failure_count=$(grep -c '1 failed' "$logs/${test_name_only}_$i.txt")
         echo "Failure_count=$failure_count"
         if [[ $failure_count -gt 0 ]]; then
             test_failure=1
-            echo "${git_link},${sha},${test_name_with_classname},test_failure,$i" >> "$Results/Test_Run_Result.csv"
+            echo "${git_link},${sha},${test_name_with_classname},test_failure,$i" >> "$Results/Test_Run_Result_tox_Compiled.csv"
             echo "I get the test failure"
+            mv coverage.xml "$coverage_Dir/${test_name_only}_$i.xml"
         else
             test_pass=1
-            echo "${git_link},${sha},${test_name_with_classname},test_pass,$i" >> "$Results/Test_Run_Result.csv"
+            echo "${git_link},${sha},${test_name_with_classname},test_pass,$i" >> "$Results/Test_Run_Result_tox_Compiled.csv"
+            mv coverage.xml "$coverage_Dir/${test_name_only}_$i.xml"
         fi
         if [[ $test_pass -eq 1 && $test_failure -eq 1 ]]; then
-            echo "${git_link},${sha},${test_name_with_classname},both_test_fail_and_test_pass,above" >> "$Results/Test_Run_Result.csv"
+            echo "${git_link},${sha},${test_name_with_classname},both_test_fail_and_test_pass,above" >> "$Results/Test_Run_Result_tox_Compiled.csv"
             break
         fi
     done 
+    git checkout "tox.ini" 
+    cd $currentDir
     exit
-   cd $currentDir 
 done < $1 
