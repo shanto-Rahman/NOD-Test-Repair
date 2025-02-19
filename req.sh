d835f71c9d2325bcbd067314eba24ac990606c3d
SHANTO <shanto.rahman@utexas.edu>
2025-02-18 20:25:07 -0600

    more readme

--- req.sh
+++ req.sh
@@ -5,3 +5,6 @@ pip install scikit-learn
 pip install tree_sitter
 pip install tree_sitter_languages
 pip install 'accelerate>=0.26.0'
+
+#pip uninstall torch torchvision torchaudio
+#pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117
