655f2ba44e047060073436939d71d6043de6475e
Rahman Shanto (sr53282) <sr53282@austin.utexas.edu@ece-a58482.austin.utexas.edu>
2025-05-07 00:30:45 -0500

    remove unused stuff

--- Java/search_for_failure_reproducing.sh
+++ Java/search_for_failure_reproducing.sh
@@ -31,22 +31,6 @@ while read line
             fail_log_csv_name=$(find logs -name "${module_with_dot}-${testName}-FlakeDelay-Run-1-*.txt") #Java-WebSocket-org.java_websocket.issues.Issue580Test#runNoCloseBlockingTestScenario8-FlakeDelay-Run-1-3200.txt
         fi
 
-
-       #chunk=$( sed -n '/Running org\.apache\.uniffle\.common\.rpc\.GrpcServerTest/,/Tests run: 1/p' $fail_log_csv_name)
-       #echo $chunk
-       # capture
-       #chunk=$( sed -n '/Running org\.apache\.uniffle\.common\.rpc\.GrpcServerTest/,/Tests run: 1/p' "$fail_log_csv_name" )
-
-       ## print with newlines intact:
-       #printf '%s\n' "$chunk"
-       ## or simply:
-       #echo "$chunk"
-
-       #exit
-
-
         python3 generating_reproducing_script.py traces/${filename}_executed_method_bodies.csv "tmp" "traces" "gpt" traces/${filename}_test_code.csv "$fail_log_csv_name" ${slug_org} $sha ${module_org} $testName_with_dot 
-        #echo "python3 generating_reproducing_script.py traces/${filename}_executed_method_bodies.csv "tmp" "traces" "gpt" traces/${filename}_test_code.csv "logs/tmp_failure.csv" ${slug_org} $sha ${module_org} $testName_with_dot" 
 
-        #python3 generating_reproducing_script.py traces/apache_incubator-uniffle_common_org.apache.uniffle.common.rpc.GrpcServerTest\#testGrpcExecutorPool_executed_method_bodies.csv "tmp"  "traces" "gpt" traces/apache_incubator-uniffle_common_org.apache.uniffle.common.rpc.GrpcServerTest\#testGrpcExecutorPool_test_code.csv "logs/tmp_failure.csv" ${slug_org} $sha ${module_org} $testName_with_dot
     done < $1
