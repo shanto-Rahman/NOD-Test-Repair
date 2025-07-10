a8d31380575783871971e62d5eefef704f89b2d7
SHANTO <shanto.rahman@utexas.edu>
2025-07-10 11:01:12 -0500

    bash script

--- Java/x.sh
+++ Java/x.sh
@@ -1,3 +1,5 @@
-#bash runAll.sh ../data/tmp.csv results/ "static"
+#bash runAll.sh ../data/talank_with_test_id_idoft.csv results/ "static"
 #bash runAll.sh ../data/tmp.csv results/ "dynamic"
-bash search_for_failure_reproducing.sh ../data/talank_with_test_id_idoft.csv X
+#bash search_for_failure_reproducing.sh ../data/talank_with_test_id_idoft.csv idoft
+bash runAll.sh ../data/talank_with_test_id_idoft.csv results/ 
+bash search_for_failure_reproducing.sh ../data/talank_with_test_id_flakerake.csv flakerake
