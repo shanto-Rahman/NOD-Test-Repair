d380fdac24bb26325e3bb36a460096b06d88a042
Rahman Shanto (sr53282) <sr53282@austin.utexas.edu@ece-a58482.austin.utexas.edu>
2025-07-17 20:36:14 -0500

    rq2

--- Java/rq2.sh
+++ Java/rq2.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 #bash re-run_baseline.sh ../data/talank_with_test_id_idoft.csv results
-if [[ $1 == "" || $2 == "" ]]; then
+if [[ $1 == "" || $2 == "" || $3 == "" ]]; then
     echo "arg1 - full path to the test file (eg. tmp.csv)"
     echo "arg2 - relative path to the output file (eg. results)"
+    echo "arg3- test-type(idoft/flakerake)"
     exit
 fi
 
@@ -44,6 +45,7 @@ while IFS= read -r line
     module=$(echo $line | cut -d',' -f3)
     testName=$(echo $line | cut -d',' -f4)
     file_meth_line_to_inject_delay=$(echo $line | cut -d',' -f5)
+    echo "file_meth_line_to_inject_delay=, $file_meth_line_to_inject_delay"
     if [[ $file_meth_line_to_inject_delay == "" ]]; then
         echo "No solutions found before"
         continue
@@ -52,7 +54,7 @@ while IFS= read -r line
     method_name=$(echo $file_meth_line_to_inject_delay | cut -d':' -f2)
     method_descriptor=$(echo $file_meth_line_to_inject_delay | cut -d':' -f3)
     line_number=$(echo $file_meth_line_to_inject_delay | cut -d':' -f4 | cut -d' ' -f1)
-    code_line=$(echo $file_meth_line_to_inject_delay | cut -d':' -f4 | cut -d' ' -f2-)
+    code_line=$(echo $file_meth_line_to_inject_delay | cut -d':' -f4 | cut -d' ' -f2- | sed 's/^(//; s/)$//')
 
     if [[ ! -d ${inputProj}/${slug} ]]; then
         git clone "https://github.com/$slug" $inputProj/$slug
@@ -69,21 +71,21 @@ while IFS= read -r line
     
     rootProj=$(echo "$slug" | cut -d/ -f 1)
     subProj=$(echo "$slug" | cut -d/ -f 2)
+    
+    	JMVNOPTIONS=""
+    if [[ "$slug" == "doanduyhai/Achilles" ]]; then
+        sed -i 's~http://repo1.maven.org/maven2~https://repo1.maven.org/maven2~g' pom.xml
+        sed -i '/<plugin>/,/<\/plugin>/ {
+         /<groupId>org.apache.felix<\/groupId>/ {
+           N
+           /<artifactId>maven-bundle-plugin<\/artifactId>/ {
+             a\
+             <version>${felix.version}</version>
+           }
+         }
+       }' pom.xml
 
-	JMVNOPTIONS=""
-    #if [[ "$slug" == "doanduyhai/Achilles" ]]; then
-    #    sed -i 's~http://repo1.maven.org/maven2~https://repo1.maven.org/maven2~g' pom.xml
-    #    sed -i '/<plugin>/,/<\/plugin>/ {
-    #     /<groupId>org.apache.felix<\/groupId>/ {
-    #       N
-    #       /<artifactId>maven-bundle-plugin<\/artifactId>/ {
-    #         a\
-    #         <version>${felix.version}</version>
-    #       }
-    #     }
-    #   }' pom.xml
-
-    if [[ $slug == "apache/dubbo" ]]; then
+    elif [[ $slug == "apache/dubbo" ]]; then
         JMVNOPTIONS="-pl dubbo-dependencies-bom"
 
     elif [[ $slug == "apache/httpcore" ]]; then
@@ -121,18 +123,40 @@ while IFS= read -r line
     echo "$code_line"
     cd $currentDir
     python3 run_injection.py "$file_path" "$line_number" "$method_name" "$method_descriptor" "$code_line" "$slug" "$module"
+    #log for baseline
+    log_search_csv=""
+    if [[ $3 == "flakerake" ]]; then #will read failure message from, and save that into a txt file similar to the name of idoft unique_failures_10K_reruns_flakerake_775.csv
+        log_search_csv="../Results/unique_failures_10K_reruns_flakerake_775.csv"
+    elif [[ $3 == "idoft" ]]; then #../Results/unique_failures_10K_reruns_181_unique_only.csv
+        log_search_csv="../Results/unique_failures_10K_reruns_181_unique_only.csv"
+    fi
+
+    module_with_dot=${module//\//.}
+    proj_name_only=$(echo $slug | cut -d'/' -f2)
+    python3 find_failure_message_and_save.py "ID" "$slug" "$sha" "$module" "$testName_with_hash" "$log_search_csv" "$module_with_dot" "$proj_name_only"
+    #id_arg, slug, sha, module_org, testName= ID  fa3909c391195178ccf5a92d4ac342a30ae247c8 . org.java_websocket.issues.Issue580Test#runNoCloseBlockingTestScenario0
+
+    if [[ $module == "." ]]; then
+        fail_log_csv_name="$logs/ID_${proj_name_only}_${testName_with_hash}_stacktrace.csv"
+    else
+        fail_log_csv_name="$logs/ID_${module_with_dot}_${testName_with_hash}_stacktrace.csv"
+    fi
     cd -
     mvn clean install -pl $module -am -DskipTests
     start=$(date +%s.%N)
-    for i in {1..100}; do
+    for i in {1..1}; do
         echo "mvn test $JMVNOPTIONS  -pl $module  -Dtest=$testName_with_hash"
-        #exit
         mvn test $JMVNOPTIONS  -pl $module  -Dtest=$testName_with_hash >  "$logs/$testName-$i.txt"
+        exit
 
         bugCount=$(grep -ic -E 'Errors: 1|Failures: 1' "$logs/$testName-$i.txt")
         if [[ $bugCount -gt 0  ]]; then
+            python3 log_similarity_init.py "$logs/$testName-$i.txt" "$fail_log_csv_name" "$testName" #python3 get_similarity_score_stacktrace
+
+            echo "$fail_log_csv_name"
             echo -n "1;" >> "$currentDir/$outputDir/RQ2-Result.csv"
             echo "Failure found."
+            #exit
         else
             echo -n "0;" >> "$currentDir/$outputDir/RQ2-Result.csv"
             echo "Failure not found."
@@ -142,5 +166,8 @@ while IFS= read -r line
     take=$(echo "scale=2; ${end} - ${start}" | bc)
     take=$(echo $take | awk '{printf("%.2f\n", $1) }')
     echo ",$take" >> "$currentDir/$outputDir/RQ2-Result.csv"
-    git checkout -- '**/*.java'
+
+    #exit
+    #git checkout -- '**/*.java'
+    git stash
 done < $1
