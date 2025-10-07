42c511cd78d53c7f7e443292a56fe6453f237b85
Rahman Shanto (sr53282) <sr53282@austin.utexas.edu@ece-a58482.austin.utexas.edu>
2025-10-07 17:00:46 -0500

    fix id extract

--- Java/find_failure_match.sh
+++ Java/find_failure_match.sh
@@ -8,7 +8,7 @@ while read line
     testName=$(echo $line | cut -d',' -f4)
     testName_with_hash=$(echo "$testName" | sed 's/\(.*\)\./\1#/') 
     test_not_failed=$(echo $line | cut -d',' -f5)
-    id_to_get_the_test_failure=$(echo $line | cut -d',' -f6)
+    id_to_get_the_test_failure=$(echo $line | rev | cut -d',' -f2 | rev)
     echo "$slug"
     echo $test_not_failed
     if [[ ${test_not_failed} == "" ]]; then
@@ -34,19 +34,18 @@ while read line
     if [[ -f "tmp.txt" ]]; then
         result=$(python3 "$currentDir/log_similarity_init.py" "tmp.txt" "$tdrepro_failure_log_file"  "$testName") #python3 get_similarity_score_stacktrace
         result=$(echo "$result" | xargs)  # removes leading/trailing whitespace
-        echo "$result"
+        #echo "$result"
         #exit
         if [[ "$result" == *"MisMatched"* ]] ; then
-            echo "**** $fail_log_csv_name"
+            #echo "**** $fail_log_csv_name"
             echo "$slug,$sha,$module,$testName_with_hash,0" >> "$currentDir/results/Reliable-Failure-Result-RQ1.csv"
-            echo "MisMatched Failure found."
+            #echo "MisMatched Failure found."
         else # [[ "$result" == "Matched" ]] ; then
-            echo "PPPPPPPPPp"
+            #echo "PPPPPPPPPp"
             echo "$slug,$sha,$module,$testName_with_hash,1" >> "$currentDir/results/Reliable-Failure-Result-RQ1.csv"
-            echo "$result Failure found."
+            #echo "$result Failure found."
         fi
-        echo "$tdrepro_failure_log_file"
-        exit
+        #echo "$tdrepro_failure_log_file"
         rm tmp.txt
    else
        echo "$slug,$sha,$module,$testName_with_hash,not_idoft" >> "$currentDir/results/Reliable-Failure-Result-RQ1.csv"
