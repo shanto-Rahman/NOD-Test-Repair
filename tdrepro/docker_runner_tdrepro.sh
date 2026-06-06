55935cd6ae267fdc3e76f7e51766380825b50b35
Rahman Shanto (sr53282) <sr53282@austin.utexas.edu@ece-a58482.austin.utexas.edu>
2026-06-06 11:55:41 -0500

    docker_runner_tdrepro

---
+++ tdrepro/docker_runner_tdrepro.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+INPUT_CSV=$(realpath "$1")
+OUTPUT_NAME="$2"
+
+if [ -z "$OUTPUT_NAME" ]; then
+    echo "Usage: bash docker_runner.sh input.csv output_name"
+    echo "Example: bash docker_runner.sh hbase_tests.csv hbase"
+    exit 1
+fi
+
+if [[ "$OUTPUT_NAME" == "hbase" ]]; then
+    IMAGE_NAME="hbase:shanto_modified_v2"
+    PROJECT_MOUNT=""
+else
+    IMAGE_NAME="docker-nod-repair-env:latest"
+    PROJECT_MOUNT="-v $(pwd)/..:/NOD-Test-Repair"
+fi
+
+mkdir -p docker_results docker_logs docker_tdrepro_gpt_responses docker_metadata
+
+docker rm -f "hbase_run_${OUTPUT_NAME}" 2>/dev/null || true
+
+docker run --rm \
+    --name "hbase_run_${OUTPUT_NAME}" \
+    -e OPENAI_API_KEY="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" \
+    -v "$INPUT_CSV:/tmp/input.csv" \
+    -v "$(pwd)/docker_results:/docker_results" \
+    -v "$(pwd)/docker_logs:/docker_logs" \
+    -v "$(pwd)/docker_tdrepro_gpt_responses:/docker_tdrepro_gpt_responses" \ 
+    -v "$(pwd)/docker_metadata:/docker_metadata" \
+    -v "$(pwd)/docker_traces/traces:/NOD-Test-Repair/tdrepro/traces:ro" \
+    $PROJECT_MOUNT \
+    "$IMAGE_NAME" \
+    bash -c "
+        set -o pipefail
+
+        . \$HOME/.profile
+        . /root/miniconda3/etc/profile.d/conda.sh
+
+        cd /NOD-Test-Repair/Results
+
+        cd /NOD-Test-Repair/tdrepro
+        git config --global --add safe.directory '*'
+
+        conda activate tdrepro || echo 'Conda activation failed'
+
+        bash search_for_failure_reproducing.sh /tmp/input.csv 
+
+        cp /NOD-Test-Repair/tdrepro/results/tdrepro.csv /docker_results/${OUTPUT_NAME}_tdrepro.csv || true
+        cp -r /NOD-Test-Repair/tdrepro/logs /docker_logs/${OUTPUT_NAME}_raw_logs_tdrepro/ || true
+        cp -r /NOD-Test-Repair/tdrepro/logs-to-reproduce /docker_logs/${OUTPUT_NAME}_raw_logs-to-reproduce_tdrepro/ || true
+        cp -r /NOD-Test-Repair/tdrepro/tdrepro_gpt_responses /docker_tdrepro_gpt_responses/${OUTPUT_NAME}_tdrepro_gpt_responses/ || true
+        cp -r /NOD-Test-Repair/tdrepro/docker_metadata /docker_metadata/${OUTPUT_NAME}_metadata/ || true
+
+        chown -R \$(stat -c '%u:%g' /docker_logs) /docker_logs /docker_results /docker_tdrepro_gpt_responses /docker_metadata || true
+    " 2>&1 | tee "docker_logs/${OUTPUT_NAME}.log" #"docker_logs/${OUTPUT_NAME}.log" 2>&1
+
+echo "All tests finished."
+echo "Results saved in docker_results/"
+echo "Logs saved in docker_logs/"
+
