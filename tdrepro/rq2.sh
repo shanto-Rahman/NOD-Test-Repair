a0652efc381bf57c855bea859d6eb83335b1b042
SHANTO <bit0321@iit.du.ac.bd>
2026-08-06 16:29:05 -0500

    script to run test with tdrepro config 100 times

--- tdrepro/rq2.sh
+++ tdrepro/rq2.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 #bash re-run_baseline.sh ../data/talank_with_test_id_idoft.csv results
-if [[ $1 == "" || $2 == "" || $3 == "" ]]; then
+if [[ $1 == "" ]]; then
     echo "arg1 - full path to the test file (eg. tmp.csv)" #tdrepro.csv
-    echo "arg2 - relative path to the output file (eg. results)" #result
-    echo "arg3- test-type(idoft/flakerake)" #"idoft"
+    #echo "arg2 - relative path to the output file (eg. results)" #result
+    #echo "arg3- test-type(idoft/flakerake)" #"idoft"
     exit
 fi
 
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 inputProj=$currentDir"/projects"
-outputDir="$2"
-trace_collection_way="$3"
+outputDir="$currentDir/results"
+#trace_collection_way="$3"
 
-if [ ! -d "$currentDir/$outputDir" ] 
+if [ ! -d "$outputDir" ] 
 then
-    mkdir "$currentDir/$outputDir"
+    mkdir "$outputDir"
 fi
 
 if [ ! -d "$inputProj" ] 
@@ -31,7 +31,7 @@ then
 fi
 
 
-echo "Project-Name,SHA,Module,Test-Name,Failure-Found,Time" > "$currentDir/$outputDir/Re-run-Baseline-Result.csv"
+echo "Project-Name,SHA,Module,Test-Name,Failure-Found,Time" > "$outputDir/Re-run-Baseline-Result.csv"
 
 while IFS= read -r line
     do
@@ -39,21 +39,13 @@ while IFS= read -r line
         echo "Line starts with Hash $line"
         continue
     fi
-    #id=$(echo $line | cut -d',' -f1)
-    slug=$(echo $line | cut -d',' -f1)
-    sha=$(echo $line | cut -d',' -f2)
-    module=$(echo $line | cut -d',' -f3)
-    testName=$(echo $line | cut -d',' -f4)
+    id=$(echo $line | cut -d',' -f1)
+    slug=$(echo $line | cut -d',' -f2)
+    sha=$(echo $line | cut -d',' -f3)
+    module=$(echo $line | cut -d',' -f4)
+    testName=$(echo $line | cut -d',' -f5)
     fifth_field=$(echo "$line" | awk -v FPAT='([^,]*)|("[^"]*")' '{print $5}')
-    # Check if it starts and ends with a double quote
-    #if [[ "$fifth_field" == \"*\" ]]; then
-    #    file_meth_line_to_inject_delay=$(awk -v FPAT='([^,]*)|("[^"]+")' '{print $5}' <<< "$line" | sed 's/^"//;s/"$//')
-    #else
-        file_meth_line_to_inject_delay=$(echo $line | cut -d',' -f5)
-    #fi
-    #file_meth_line_to_inject_delay=$(awk -v FPAT='([^,]+)|(\"[^\"]+\")' '{print $5}' <<< "$line" | sed 's/^"//;s/"$//')
-    #file_meth_line_to_inject_delay=$(echo "$line" | perl -pe 's/^([^,]*,){4}//; s/,(?:[^,]*,){2}[^,]*$//' | sed 's/^"//;s/"$//')
-
+    file_meth_line_to_inject_delay=$(echo $line | cut -d',' -f6)
 
     echo "file_meth_line_to_inject_delay=, $file_meth_line_to_inject_delay"
     if [[ $file_meth_line_to_inject_delay == "" ]]; then
@@ -72,6 +64,11 @@ while IFS= read -r line
     echo "$method_descriptor"
     echo "line_number=$line_number"
     echo "code_line=$code_line"
+
+    echo "test=$testName"
+    testName_with_hash=$(echo "$testName" | sed 's/\(.*\)\./\1#/') 
+    echo "testName= $testName_with_hash"
+
     if [[ ! -d ${inputProj}/${slug} ]]; then
         git clone "https://github.com/$slug" $inputProj/$slug
     fi
@@ -79,54 +76,32 @@ while IFS= read -r line
     cd $inputProj/$slug
     git checkout ${sha}
 
-    echo "test=$testName"
-    testName_with_hash=$(echo "$testName" | sed 's/\(.*\)\./\1#/') 
     #testName="${testName_with_dot%.*}#${testName_with_dot##*.}"
-    echo "testName= $testName_with_hash"
     testClass="$(echo $testName | rev | cut -d'.' -f2 | rev)"
     
     rootProj=$(echo "$slug" | cut -d/ -f 1)
     subProj=$(echo "$slug" | cut -d/ -f 2)
     
     JMVNOPTIONS=""
-    echo -n "${slug},${sha},${module},${testName}," >> "$currentDir/$outputDir/RQ2-Result.csv"
-    
-    if [[ $module != "." ]]; then
-        projName=$(sed 's;/;.;g' <<< $module-$testName)
-     else   
-        projName=$(sed 's;/;.;g' <<< $subProj-$testName)
-    fi
-    
-    #if [[ $slug == "Accenture/mercury" ]]; then
-    #    mvn install -pl $module -am -Dmaven.test.skip=true
-    #else
-    #    mvn install -pl $module -am -DskipTests
-    #fi
-    #slug_with_underscore="${slug//\//_}"
-    #module_with_underscore="${module//\//_}"
-    #echo "$slug_with_underscore"
-
+    echo -n "${slug},${sha},${module},${testName}," >> "$outputDir/RQ2-Result.csv"
+     
     cd $currentDir
     python3 run_injection.py "$class_name" "$line_number" "$method_name" "$method_descriptor" "$code_line" "$slug" "$module"
-    #log for baseline
-    log_search_csv=""
-    if [[ $3 == "flakerake" ]]; then #will read failure message from, and save that into a txt file similar to the name of idoft unique_failures_10K_reruns_flakerake_775.csv
-        log_search_csv="../Results/unique_failures_10K_reruns_flakerake_775.csv"
-    elif [[ $3 == "idoft" ]]; then #../Results/unique_failures_10K_reruns_181_unique_only.csv
-        log_search_csv="../Results/unique_failures_10K_reruns_181_unique_only.csv"
-    fi
+    #exit
 
     module_with_dot=${module//\//.}
     proj_name_only=$(echo $slug | cut -d'/' -f2)
     #python3 find_failure_message_and_save.py "RQ2" "$slug" "$sha" "$module" "$testName_with_hash" "$log_search_csv" "$module_with_dot" "$proj_name_only"
     ##id_arg, slug, sha, module_org, testName= ID  fa3909c391195178ccf5a92d4ac342a30ae247c8 . org.java_websocket.issues.Issue580Test#runNoCloseBlockingTestScenario0
-    if [[ ${module_with_dot} == "." ]]; then
-        fail_log_csv_name="${proj_name_only}_${testName_with_hash}_stacktrace.csv"
+
+    if [[ $module_org == "." ]]; then
+        fail_log_csv_name="$currentDir/logs/${id}_${proj_name_only}_${testName_with_hash}_stacktrace.csv"
     else
-        fail_log_csv_name="${module_with_dot}_${testName_with_hash}_stacktrace.csv"
+        fail_log_csv_name="$currentDir/logs/${id}_${module_with_dot}_${testName_with_hash}_stacktrace.csv"
     fi
-    echo "fail_log_csv_name=$fail_log_csv_name"
-    org_fail_csv=$(find $currentDir/logs -name "*$fail_log_csv_name")
+
+    #echo "fail_log_csv_name=$fail_log_csv_name"
+    #org_fail_csv=$(find $currentDir/logs -name "*$fail_log_csv_name")
     cd -
     mvn clean install -pl $module -am -DskipTests
     start=$(date +%s.%N)
@@ -136,27 +111,28 @@ while IFS= read -r line
 
         bugCount=$(grep -ic -E 'Errors: 1|Failures: 1' "$logs/$testName-$i.txt")
         if [[ $bugCount -gt 0  ]]; then
-            result=$(python3 "$currentDir/log_similarity_init.py" "$org_fail_csv" "$logs/$testName-$i.txt"  "$testName") #python3 get_similarity_score_stacktrace
+            result=$(python3 "$currentDir/log_similarity_init.py" "$fail_log_csv_name" "$logs/$testName-$i.txt"  "$testName") #python3 get_similarity_score_stacktrace
             #result=$(echo "$result" | xargs)  # removes leading/trailing whitespace
-            #echo "$result"
-	    echo "Comparing "$org_fail_csv" and "$logs/$testName-$i.txt""
+            echo "$result"
+	        echo "Comparing "$org_fail_csv" and "$logs/$testName-$i.txt""
             if [[ "$result" == *MisMatched* ]] ; then
-                echo -n "2;" >> "$currentDir/$outputDir/RQ2-Result.csv"
+                echo -n "2;" >> "$outputDir/RQ2-Result.csv"
                 echo "MisMatched Failure found."
             #elif [[ "$result" == *Matched* ]] ; then
             else
-               echo -n "1;" >> "$currentDir/$outputDir/RQ2-Result.csv" #Mismatched
+               echo -n "1;" >> "$outputDir/RQ2-Result.csv" #Mismatched
                echo "$result Matched Failure found."
 	    fi
         else
-            echo -n "0;" >> "$currentDir/$outputDir/RQ2-Result.csv" #No fail
+            echo -n "0;" >> "$outputDir/RQ2-Result.csv" #No fail
             echo "Failure not found."
         fi
     done
     end=$(date +%s.%N)
     take=$(echo "scale=2; ${end} - ${start}" | bc)
     take=$(echo $take | awk '{printf("%.2f\n", $1) }')
-    echo ",$take" >> "$currentDir/$outputDir/RQ2-Result.csv"
+    echo ",$take" >> "$outputDir/RQ2-Result.csv"
     git checkout -- '**/*.java'
+    exit
     #git stash
 done < $1
